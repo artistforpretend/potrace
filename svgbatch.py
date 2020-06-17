c10f3b1ee9ce21cf42c68ee53c918c7caa0e8691
Artistforpretend <40501114+artistforpretend@users.noreply.github.com>
2020-06-17 17:00:08 -0500

    Create svgbatch.py

---
+++ svgbatch.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+os.chdir('C:\\Users\\username\\bmpfolder')
+p = Path.cwd()
+filelist = list(p.glob('*.bmp'))
+
+for file in filelist:
+    command = f"potrace -s {file} -o {os.path.splitext(file)[0]}.svg"
+    os.system(command)
